189e872cb9d255d05573e2f1c0a9cb9fe73d479b
Martin Kutzner <mfkutz@gmail.com>
2022-12-14 22:19:53 -0300

    correccion menu

--- js/main.js
+++ js/main.js
@@ -1,12 +1,15 @@
 const botonOpenMenu = document.getElementById("open-menu");
 const botonCloseMenu = document.getElementById("close-menu");
 const openMenu = document.getElementById("mobile-menu");
+const main = document.getElementById("main");
 
 botonOpenMenu.addEventListener("click", () => {
     openMenu.classList.remove("disabled");
+    main-classList.add("disabled");
 });
 
 botonCloseMenu.addEventListener("click", ()=>{
     openMenu.classList.add("disabled");
+    main-classList.remove("disabled");
 })
 
